f006329d65fd4f8f64c38f51eac30db91258f1f2
kashikichi <aritramaji14@outlook.com>
2021-09-29 21:26:04 +0530

    halfEntry
    implemented some features for authentication v1

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,7 +1,6 @@
 function GetCookie()
 {
     let biscuit = document.cookie;
-    console.log(biscuit);
     biscuit = biscuit.split(';');
     let biscuit_s = {};
     biscuit.forEach((value)=>{
@@ -9,4 +8,45 @@ function GetCookie()
         biscuit_s[value[0].trim()] = value[1].trim();
     });
     return biscuit_s;
+}
+function CreateFormData(obj)
+{
+    const Frm = new FormData();
+    Object.keys(obj).forEach((key)=>{
+        Frm.append(key,obj[key]);
+    });
+    return Frm;
+}
+/**
+ * 
+ * @param {string} type - GET / POST
+ * @param {object} headers
+ * @param {string} Url 
+ * @param {any} RequestBody 
+ * @param {boolean} asyncronous 
+ * @param {function} callingFunction 
+ */
+function PerformAjaxRequest(
+    type , 
+    headers,
+    Url,
+     RequestBody,
+     asyncronous = true,callingFunction = null)
+{
+
+let AjxReq = new XMLHttpRequest();
+AjxReq.onreadystatechange = ()=>{
+if(AjxReq.readyState == 4 && AjxReq.status == 200)
+{
+    if(callingFunction != null)
+    {
+        callingFunction(AjxReq.response);
+    }
+}
+}
+AjxReq.open(type,Url,asyncronous);
+Object.keys(headers).forEach((key)=>{
+    AjxReq.setRequestHeader(key,headers[key]);
+    })
+AjxReq.send(RequestBody);
 }
---
+++ scripts/userentry.js
@@ -0,0 +1,72 @@
+function CheckValidPass(str)
+{
+    const MainStr = str;
+    const minLen = 6;
+    const regExAlphabets = /([a-z]|[A-Z])/;
+    const regExSymbol = /[$%@]/;
+    if(MainStr.length < minLen)
+    {
+        return `Password minimum length should be at least ${minLen}`;
+    }
+    else if(!regExAlphabets.test(MainStr))
+    {
+        return "Passwod should contain atleast 1 alphabet";
+    }
+    else if(!regExSymbol.test(MainStr))
+    {
+        return "Password should contain atleast 1 special character from @,%,$";
+    }
+    else
+    {
+        return true;
+    }
+}
+function GetUserDetails()
+{
+    const userName = document.getElementById("userNM").value.trim();
+    const userPS = document.getElementById("userPS").value.trim();
+    if(userName.length != "")
+    {
+        let res = null;
+        if((res = CheckValidPass(userPS)) == true)
+        {
+            return {"name":userName,"pass":userPS};
+        }
+        else
+        {
+            return res;
+        }
+    }
+    else
+    {
+        return "Username should not be empty";
+    }
+}
+document.onload = ()=>{
+    if(GetCookie().hasOwnProperty("active_user_id"))
+    {
+        window.location = "userarea.html";
+    }
+};
+document.getElementById("LogInBtn").onclick = ()=>{
+    const UserDtls = GetUserDetails();
+    if(typeof UserDtls == "string")
+    {
+        alert(UserDtls);
+    }
+    else
+    {
+        console.log(CreateFormData(UserDtls));
+    }
+};
+document.getElementById("SignUpBtn").onclick = ()=>{
+    const UserDtls = GetUserDetails();
+    if(typeof UserDtls == "string")
+    {
+        alert(UserDtls);
+    }
+    else
+    {
+        
+    }
+};
