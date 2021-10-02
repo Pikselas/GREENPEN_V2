ef5f96d397b4791c085b5b52be6c3b7882b14440
kashikichi <aritramaji14@outlook.com>
2021-10-02 21:40:52 +0530

    useME2
    2nd struct

--- scripts/userarea.js
+++ scripts/userarea.js
@@ -1,8 +1,13 @@
 var UserCookie = {};
 document.body.onload = ()=>{
+    console.log()
     if(!(UserCookie = GetCookie()).hasOwnProperty("active_user_name"))
     {
         window.location = "home.html";
     }
     document.getElementById("userProfilePic").src = UserCookie["active_user_profile_pic"].replaceAll("%2F","/");
+    document.getElementById("userTitle").innerHTML = UserCookie["active_user_name"];
 };
+document.getElementById("Profile").onclick = ()=>{
+    window.location = "userprofile.html";
+}
