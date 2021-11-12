1ab231071665d0e197e409476e1fb6f14c2be801
kashikichi <60090083+Pikselas@users.noreply.github.com>
2021-11-12 15:48:19 +0530

    ImageFrame
    Creating image frames

--- scripts/greeneditor.js
+++ scripts/greeneditor.js
@@ -3,18 +3,30 @@ function OnChildDrag(ev)
     ev.dataTransfer.setData("ChildID",ev.target.id);
     ev.dataTransfer.setData("x",ev.layerX);
     ev.dataTransfer.setData("y",ev.layerY);
+    ev.target.style.opacity = 0;
 }
-
 document.getElementById("ProjectArea").ondragover = (ev)=>{
     ev.preventDefault();
 };
 document.getElementById("ProjectArea").ondrop = (ev)=>{
+
     let TempChild = document.getElementById(ev.dataTransfer.getData("ChildID"));
     let Parent = document.getElementById("ProjectArea");
-    let ActualX = ev.layerX - ev.dataTransfer.getData("x");
-    let ActualY = ev.layerY - ev.dataTransfer.getData("y");
+    let ActualX = ev.pageX - ev.dataTransfer.getData("x");
+    let ActualY = ev.pageY - ev.dataTransfer.getData("y");
     TempChild.style.top = ActualY + "px";
     TempChild.style.left = ActualX + "px";
-    // Parent.removeChild(TempChild);
-    // Parent.appendChild(TempChild);
+    Parent.removeChild(TempChild);
+    Parent.appendChild(TempChild);
+    TempChild.style.opacity = 1;
+}
+function CreateImageSection()
+{
+    let MainPanel = document.createElement("div");
+    MainPanel.className = "ImagePanel";
+    MainPanel.draggable = true;
+    MainPanel.setAttribute("ondragover","HideOnDragOver(event)");
+    MainPanel.setAttribute("ondragstart","OnChildDrag(event)");
+    MainPanel.id = (Math.random() + 1).toString(36).substring(7);
+    document.getElementById("ProjectArea").appendChild(MainPanel);
 }
