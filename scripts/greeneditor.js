e628dc3ac402fd02bcecaaadb920042af38b7ac8
kashikichi <60090083+Pikselas@users.noreply.github.com>
2021-11-24 02:44:18 +0530

    Change Me baby
    Updated script structure ,
    Trust ME  JSON is awesome

--- scripts/greeneditor.js
+++ scripts/greeneditor.js
@@ -1,5 +1,8 @@
 var PROJECT_JSON = {};
 var DefaultPath = "";
+var NewAdd = {"VIDEOS" : {} , "FOLDERS" : {}};
+var Changes = {} ; // { ID1 : {"source" : "","dest":"" },ID2 : {"source" : "","dest":"" }}
+var TempFileS = {}; // {ID : {blob : "",dest : "real_path"}}
 window.addEventListener("dragover",function(e){
     e = e || event;
     e.preventDefault();
@@ -80,6 +83,7 @@ function GetAddPanel()
                 ev.dataTransfer.setData("ChildID",Img.id);
             }
             AddPanel.parentElement.children[2].appendChild(Img);
+            PROJECT_JSON["MAGES"][Img.id] = {"path" : Img.src , "path_type" : "URL"};
         }
     }
     AddPanel.onclick = (ev)=>{
@@ -100,6 +104,16 @@ function GetAddPanel()
                         ev.dataTransfer.setData("ChildID",Img.id);
                     }
                     ev.target.parentElement.children[2].appendChild(Img);
+                    let Obj = {};
+                    Obj["path"] = ev.target.parentElement.id + '/' + FileList.files[i].name;
+                    Obj["path_type"] = "LOCAL";
+                    PROJECT_JSON["IMAGES"][Img.id] = Obj;
+                    PROJECT_JSON["IMAGE_FRAMES"][ev.target.parentElement.id]["IMAGE_LIST"][Img.id] = "";
+                    Obj = {"blob" : FileList.files[i] , "dest" : Obj["path"]};
+                    TempFileS[Img.id] = Obj;
+                    setTimeout(()=>{
+                        URL.revokeObjectURL(Img.src);
+                    },10);
                 }
             }
             FileList.click();
@@ -107,8 +121,38 @@ function GetAddPanel()
     }
     AddPanel.ondrop = (ev) =>{
         let TempChild = document.getElementById(ev.dataTransfer.getData("ChildID"));
+        delete PROJECT_JSON["IMAGE_FRAMES"][TempChild.parentElement.parentElement.id]["IMAGE_LIST"][TempChild.id];
+        PROJECT_JSON["IMAGE_FRAMES"][ev.target.parentElement.id]["IMAGE_LIST"][TempChild.id] = "";
         TempChild.parentElement.removeChild(TempChild);
         ev.target.parentElement.children[2].appendChild(TempChild)
+        if(PROJECT_JSON["IMAGES"][TempChild.id]["path_type"] != "URL")
+        {
+            let NewPath = ev.target.parentElement.id + '/' + PROJECT_JSON["IMAGES"][TempChild.id]["path"].split('/').reverse()[0];
+            console.log(NewPath);
+            // checking if it is a temp file ?
+            // if so then no need to put it in changes ,instead change the dest url
+            // else put it to changes
+            if( TempFileS.hasOwnProperty(TempChild.id))
+            {
+                TempFileS[TempChild.id]["dest"] = NewPath;
+            }
+            else
+            {
+                //we have made a change
+                if(Changes.hasOwnProperty(TempChild.id))
+                {
+                    //so if it's already in changes means we don't need to make a change 
+                    // on source beacuse source is same in server side
+                    Changes[TempChild.id]["dest"] = NewPath;
+                }
+                else
+                {
+                    //this is first time we changing so we are adding a new change
+                Changes[TempChild.id] = {"source" : PROJECT_JSON["IMAGES"][TempChild.id]["path"],
+                                                "dest" : NewPath};
+                }
+            }
+      }
     }
     AddPanel.appendChild(AddPanelInput);
     return AddPanel;
@@ -155,6 +199,7 @@ function CreateImageSection(ID,width = null,height = null , Left , Top)
     //when the X button will be clicked the frame will be removed including all the images
     PanelCloseButton.onclick = (ev)=>{
         document.getElementById("ProjectArea").removeChild(ev.target.parentElement);
+        RemoveImageSection(ID);
     }
     PanelAddButton.className = "ImageFrameAddButton";
     PanelAddButton.innerHTML = "+";
@@ -227,18 +272,38 @@ function AddNewVideoSection()
    ID = (Math.random() + 1).toString(36).substring(7);
    ParentItem.appendChild(CreateVideoItem(ID,null,null,Left,Top));
 }
+function RemoveImageSection(ID)
+{
+   Object.keys(PROJECT_JSON["IMAGE_FRAMES"][ID]["IMAGE_LIST"]).forEach((ky)=>{
+    delete PROJECT_JSON["IMAGES"][ky];
+    if(TempFileS.hasOwnProperty(ky))
+    {
+        delete TempFileS[ky];
+    }
+    if(Changes.hasOwnProperty(ky))
+    {
+        delete Changes[ky];
+    }
+   });
+   if(NewAdd["FOLDERS"].hasOwnProperty(ID))
+   {
+       delete NewAdd["FOLDERS"][ID];
+   }
+   delete PROJECT_JSON["IMAGE_FRAMES"][ID];
+}
 function AddNewImageSection()
 {
    let ParentItem = document.getElementById("ProjectArea");
    Left = Math.floor(Math.random() * (ParentItem.offsetWidth - (ParentItem.offsetWidth * 30 / 100))) + "px";
    Top = Math.floor(Math.random() * (ParentItem.offsetHeight - (ParentItem.offsetHeight * 40 / 100))) + "px";
    ID = (Math.random() + 1).toString(36).substring(7);
-   PROJECT_JSON["IMAGES"][ID] = {};
-   PROJECT_JSON["IMAGES"][ID]["LEFT"] = Left;
-   PROJECT_JSON["IMAGES"][ID]["TOP"] = Top;
-   PROJECT_JSON["IMAGES"][ID]["WIDTH"] = null;
-   PROJECT_JSON["IMAGES"][ID]["HEIGHT"] = null;
-   PROJECT_JSON["IMAGES"][ID]["IMAGE_LIST"] = [];
+   PROJECT_JSON["IMAGE_FRAMES"][ID] = {};
+   PROJECT_JSON["IMAGE_FRAMES"][ID]["LEFT"] = Left;
+   PROJECT_JSON["IMAGE_FRAMES"][ID]["TOP"] = Top;
+   PROJECT_JSON["IMAGE_FRAMES"][ID]["WIDTH"] = null;
+   PROJECT_JSON["IMAGE_FRAMES"][ID]["HEIGHT"] = null;
+   PROJECT_JSON["IMAGE_FRAMES"][ID]["IMAGE_LIST"] = {};
+   NewAdd["FOLDERS"][ID] = "";
    ParentItem.appendChild(CreateImageSection(ID,null,null,Left,Top));
 }
 function Save()
