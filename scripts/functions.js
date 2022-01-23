0c4d83db72e8732fb78a1c2987b5c5a842636a3b
kashikichi <60090083+Pikselas@users.noreply.github.com>
2022-01-23 17:17:34 +0530

    Scroll_tools
    Some styles ,
    Fullscreen

--- scripts/functions.js
+++ scripts/functions.js
@@ -80,3 +80,13 @@ function GetPathData()
     })
     return ResultJson;
 }
+function EnterFullScreen(elem)
+{
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+      } else if (elem.webkitRequestFullscreen) { /* Safari */
+        elem.webkitRequestFullscreen();
+      } else if (elem.msRequestFullscreen) { /* IE11 */
+        elem.msRequestFullscreen();
+      }
+}
