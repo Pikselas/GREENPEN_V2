8ff161a4f85258ec2a71a16e04a8d2c3d9758c2a
kashikichi <60090083+Pikselas@users.noreply.github.com>
2022-02-09 00:03:06 +0530

    ProjectDetails

--- scripts/greenroom.js
+++ scripts/greenroom.js
@@ -10,10 +10,25 @@ document.body.onload = ()=>{
                 document.getElementById("ProjectLgT").children[0].src = response["POSTER"];
                 document.getElementById("ProjectLgT").children[1].innerHTML = response["NAME"];
                 ProjectCode = response["CODE"];
+                delete response["success"];
+                delete response["POSTER"];
+                let dtlsArea = document.getElementById("ProjectDtls");
+                let Table = document.createElement("table");
+                let AuthorRow = document.createElement("tr");
+                AuthorRow.innerHTML = `<td>AUTHOR-ID</td><td>${response["AUTHORID"]}</td>`;
+                let LastUpdateRow = document.createElement("tr");
+                LastUpdateRow.innerHTML = `<td>LAST-UPDATED</td>${response["UPDATED"]}<td></td>`;
+                let ProjectCodeRow = document.createElement("tr");
+                ProjectCodeRow.innerHTML = `<td>PROJECT-CODE</td>${response["CODE"]}<td></td>`
+                Table.appendChild(AuthorRow);
+                Table.appendChild(LastUpdateRow);
+                Table.appendChild(ProjectCodeRow)
+                dtlsArea.appendChild(Table);
+                console.log(response);
             }
         });
     }
 }
-document.getElementById("ProjectAction").children[0].onclick = ()=>{
+document.getElementById("OpenButton").onclick = ()=>{
     window.open("greeneditor.html?code="+ProjectCode);
 }
